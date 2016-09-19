c53ac2b9251c184cef7afb2c18d382913c34949d
West Brown <lawsonsdad@gmail.com>
2016-09-19 19:35:57 -0400

    tweaking site

--- resources/views/welcome/what.blade.php
+++ resources/views/welcome/what.blade.php
@@ -111,7 +111,17 @@
 	</div>
 </div><!-- /container -->
 
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
+</script>
 
 <script>
 // Get the modal
