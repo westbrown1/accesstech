75aaa28204c1e8034227ed31874730851e4a941b
West Brown <lawsonsdad@gmail.com>
2016-09-19 00:31:02 -0400

    added solution page

---
+++ resources/views/welcome/solution.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', '| Why Us?')
+
+@section('content')
+<div class="row">
+	<div class="col-md-10 col-md-offset-1 well solution">
+		<h2 class="text-center" id="eleven">11 Reasons To Choose Us</h2>
+		<ol class="text-justify reasons">
+			<li>Our designs are fully responsive.  They scale great with smart phones, tablets and any size computer. 1.5. We are super with animation (appearance of movement).</li>
+			<li>We use high quality, newer, fast Virtual Private Servers (nginx, Ubuntu 16.04 x 64 bit (production stack)).  It only costs $5 per month for a standard business website.  You'll have your own space RESERVED on a private server 24/365.  This will allow fast, dependable, quality service.</li>
+			<li>We set up your server completely for a turnkey site, all you have to do is search for it online.  This will save you +-$20 per month.</li>
+			<li>We advise against shared hosting.  Shared hosting companies typically cram 1,000's of sites on an old, average quality server.  It's basically first come first serve.  When a lot of people come to these sites at the same time, these servers typically go down.  It costs $4-$6 per month for a single website.  See #2 above... right?  But if you want, we will accommodate.</li>						
+			<li>We develop Wordpress and Drupal websites.  These CMS's (Content Management Systems) do allow for quick and quality website development.  They provide easy to use backends where you can manage the frontend.  However, they do have customizing limitations.</li>
+			<li>We only develop on OS X (Apple).  I know, I've had many different computers with different operating systems, and they were all great!  But when it comes to web development, trust me, well you know... If we don't break, you won't have to wait.</li>
+			<li>We like to work smart.  We work with Sublime Text 3 IDE (integrated development environment) and use the top plugins including Emmet. We use custom snippets, key bindings, macros and of course, Sublime's super autocompletion.  This allows for much quicker coding without losing quality.</li>
+			<li>We develop with top frameworks such as Ruby on Rails (Ruby language) and Laravel (PHP language).  Developing custom websites is sooooo much faster when using high quality, well supported frameworks.  These frameworks get faster every day.  They are a must if you are not using Wordpress or Drupal.</li>
+			<li>We do not charge for any time it takes to fix our mistakes (a given right?) or for researching a solution... be careful out there.</li>
+			<li>We develop with mobile first responsive Bootstrap CSS (Cascading Style Sheets) and SCSS (Sassy CSS).  This allows fast, well organized, easy to manage code.</li>
+			<li>if you like, we will be honored to manage your website for you. We really know SEO. Check out our what we can do page <a href="/what" class="linkspacing">Here</a> | <a href="/pricing" class="linkspacing">Back to Pricing</a> | <a href="/" class="linkspacing">Home</a></li>
+						
+		</ol>
+	</div>
+</div>
+@stop
