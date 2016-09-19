a53bef66de5eefca0823863cb8f3c83868f7be65
West Brown <lawsonsdad@gmail.com>
2016-09-19 17:43:08 -0400

    tweaking site

--- resources/views/welcome/what.blade.php
+++ resources/views/welcome/what.blade.php
@@ -94,8 +94,75 @@
 			</div>				
 		</div>
 		<div class="row">
-			<div class="col-md-12">
-				<img src="/images/google-chrome-icon-2.png" alt="google chrome icon" class="google"><h5 id="googtext">hover over Google</h5>
+			<div class="col-md-5">
+				<img src="/images/google-chrome-icon-2.png" alt="google chrome icon" class="google"><h5 id="googtext">hover over Google Chrome icon</h5>
+			</div>
+			<div class="col-md-6">
+				<div class="col-md-8">
+				<h2 style="text-align:center">Lightbox</h2>
+
+				<div class="row">
+				  <div class="column">
+				    <img src="/images/photo-1471922694854-ff1b63b20054.jpeg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+				  </div>
+				  <div class="column">
+				    <img src="/images/photo-1465152251391-e94453ee3f5a.jpeg" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+				  </div>
+				  <div class="column">
+				    <img src="/images/photo-1461770354136-8f58567b617a.jpeg" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+				  </div>
+				  <div class="column">
+				    <img src="/images/photo-1433567212640-211efabc03e1 (1).jpeg" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+				  </div>
+				</div>
+
+				<div id="myModal" class="modal">
+				  <span class="close cursor" onclick="closeModal()">&times;</span>
+				  <div class="modal-content">
+
+				    <div class="mySlides">
+				      <div class="numbertext">1 / 4</div>
+				      <img src="/images/photo-1471922694854-ff1b63b20054.jpeg" style="width:100%">
+				    </div>
+
+				    <div class="mySlides">
+				      <div class="numbertext">2 / 4</div>
+				      <img src="/images/photo-1465152251391-e94453ee3f5a.jpeg" style="width:100%">
+				    </div>
+
+				    <div class="mySlides">
+				      <div class="numbertext">3 / 4</div>
+				      <img src="/images/photo-1461770354136-8f58567b617a.jpeg" style="width:100%">
+				    </div>
+				    
+				    <div class="mySlides">
+				      <div class="numbertext">4 / 4</div>
+				      <img src="/images/photo-1433567212640-211efabc03e1 (1).jpeg" style="width:100%">
+				    </div>
+				    
+				    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+				    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+				    <div class="caption-container">
+				      <p id="caption"></p>
+				    </div>
+
+
+				    <div class="column">
+				      <img class="demo cursor" src="/images/photo-1471922694854-ff1b63b20054.jpeg" style="width:100%" onclick="currentSlide(1)" alt="Ocean at Dawn">
+				    </div>
+				    <div class="column">
+				      <img class="demo cursor" src="/images/photo-1465152251391-e94453ee3f5a.jpeg" style="width:100%" onclick="currentSlide(2)" alt="Surfer">
+				    </div>
+				    <div class="column">
+				      <img class="demo cursor" src="/images/photo-1461770354136-8f58567b617a.jpeg" style="width:100%" onclick="currentSlide(3)" alt="Ocean at Sunset">
+				    </div>
+				    <div class="column">
+				      <img class="demo cursor" src="/images/photo-1433567212640-211efabc03e1 (1).jpeg" style="width:100%" onclick="currentSlide(4)" alt="Rocky Coast">
+				    </div>
+				  </div>
+				</div>
+			</div>
 			</div>
 		</div>
 	</div><!-- /container -->
@@ -109,6 +176,45 @@
 		</div></center>
 	</div>
 </div><!-- /container -->
+
+<script>
+	function openModal() {
+	  document.getElementById('myModal').style.display = "block";
+	}
+
+	function closeModal() {
+	  document.getElementById('myModal').style.display = "none";
+	}
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("demo");
+	  var captionText = document.getElementById("caption");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	  captionText.innerHTML = dots[slideIndex-1].alt;
+	}
+</script>
 
 <script>
 // Get the modal
