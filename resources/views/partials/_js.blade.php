ba067f7b15550ab01129528b62c022fbfb096921
West Brown <lawsonsdad@gmail.com>
2016-09-19 21:32:27 -0400

    tweaking site

--- resources/views/partials/_js.blade.php
+++ resources/views/partials/_js.blade.php
@@ -3,6 +3,8 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+<script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
+
 <script>
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
