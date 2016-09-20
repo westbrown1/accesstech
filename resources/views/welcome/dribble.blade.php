8a31ef4ee9c5c9cf44a656b844749b5ec2baf031
West Brown <lawsonsdad@gmail.com>
2016-09-19 21:22:36 -0400

    tweaking site

--- resources/views/welcome/dribble.blade.php
+++ resources/views/welcome/dribble.blade.php
@@ -7,7 +7,7 @@
   <div class="container">
     <div class="website">
       <div class="header">
-        <div class="logo"> </div>
+        <div class="grid"></div>
         <div class="text">CSS Animation Build In</div>
       </div>
       <div class="grid">
