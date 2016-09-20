df95bf7435dcd47d2a45600e3a8458355edf32f0
West Brown <lawsonsdad@gmail.com>
2016-09-20 03:01:27 -0400

    tweaking site

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -8,8 +8,7 @@
 #ocean {
 	background-image: url("https://hd.unsplash.com/photo-1415018255745-0ec3f7aee47b");
 	background-size: cover;
-	background-position: center center;
-	background-attachment: fixed;
+	background: 100vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
