85273f5e08e525de976e3ad43c906c2509b77ea3
West Brown <lawsonsdad@gmail.com>
2016-09-21 16:52:39 -0400

    updated site

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -9,6 +9,6 @@
 		<a href="/solution">Why Choose Us?</a>
 		<a href="/what">A Sample Of What We Can Do</a>
 		<a href="/dribble">Load Effect Sample</a>
-		<a href="/part2">Play The Aviator (By Karim Maaloul)</a>
+		{{-- <a href="/part2">Play The Aviator (By Karim Maaloul)</a> --}}
 
 </div>
