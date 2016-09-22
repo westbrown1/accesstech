b9a4811f5b6aa535cc1a4f377200ff9c04373bd0
West Brown <lawsonsdad@gmail.com>
2016-09-22 14:54:39 -0400

    updated site

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -17,13 +17,13 @@
 <div class="container">
 <body id="ocean">
 <div class="row">
-	<div class="col-md-4 col-md-offset-4">
+	<center><div class="col-md-4 col-md-offset-4">
 				<!-- popup -->
 		<div id="popup">
 			 <h4 class="text-center" id="brand">AccessTech.io</h4>
 			 <h5 class="text-center">Let Us Build An Awesome Website For You !</h5>
 		</div><!-- /popup -->
-	</div>
+	</div></center>
 </div>
 </div>
 </body>
