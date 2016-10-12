d944b6745feddfc2cb803b48e894907cc328e0c0
West Brown <lawsonsdad@gmail.com>
2016-10-12 17:20:34 -0400

    added pricing table examples

--- resources/views/welcome/pricing-table.blade.php
+++ resources/views/welcome/pricing-table.blade.php
@@ -3,7 +3,7 @@
 @section('title', '| Pricing Table Examples')
 
 @section('content')
-    <head>
+
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,7 +27,7 @@
         <!--[if IE]>
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-    </head>
+
     <div class="container">
         <header class="codrops-header" id="codrops">
             <h1>Pricing Tables</h1>
