c6a29c902f39da8fdcc34e8d9ebad12ee22a3b0e
West Brown <lawsonsdad@gmail.com>
2016-10-12 18:59:37 -0400

    added pricing table examples

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', '| Home')
+
+@section('content')
+<style>
+
+#ocean {
+	background-image: url("https://hd.unsplash.com/photo-1415018255745-0ec3f7aee47b");
+	background-size: cover;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;	
+}
+
+</style>
+<div class="container">
+<body id="ocean">
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+				<!-- popup -->
+		<center><div id="popup">
+			 <h4 class="text-center" id="brand">AccessTech.io</h4>
+			 <h5 class="text-center">Let Us Build An Awesome Website For You !</h5>
+		</div><!-- /popup --></center>
+	</div>
+</div>
+</div>
+</body>
+@stop
