04475684c7393116468e8d27fda9b7eff0af8511
West Brown <lawsonsdad@gmail.com>
2016-10-12 21:28:41 -0400

    added pricing table examples

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -24,7 +24,7 @@
             <div class="content">
                 <div id="large-header" class="large-header">
                     <canvas id="demo-canvas"></canvas>
-                    <h1 class="main-title">AccessTech.io</span></h1>
+                    <h1 class="main-title" id="popup">AccessTech.io</span></h1>
                 </div>
             </div>
        </div>
