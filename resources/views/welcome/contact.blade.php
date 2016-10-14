91380994cfc8059e87026670daf9753314adb4dd
West Brown <lawsonsdad@gmail.com>
2016-10-14 00:08:16 -0400

    changed text color

--- resources/views/welcome/contact.blade.php
+++ resources/views/welcome/contact.blade.php
@@ -117,7 +117,7 @@ var marker=new google.maps.Marker({
 marker.setMap(map);
 
 var infowindow = new google.maps.InfoWindow({
-content: '<div class="map">AccessTech.io website design and development Wilmington NC, 28409</div>'
+content: '<div class="map">AccessTech.io website design and development Wilmington NC, 28409 lawsonsdad@gmail.com</div>'
 });
 infowindow.open(map,marker);
 }
