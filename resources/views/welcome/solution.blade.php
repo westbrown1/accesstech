633909afcfd5a539f692b6e42f52fe22deee6ff2
West Brown <lawsonsdad@gmail.com>
2016-10-13 20:14:19 -0400

    changed text color

--- resources/views/welcome/solution.blade.php
+++ resources/views/welcome/solution.blade.php
@@ -3,20 +3,6 @@
 @section('title', '| Why Us?')
 
 @section('content')
-<head>
-	<meta name="description" content="Adding elasticity with SVG shape animations" />
-	<meta name="keywords" content="svg, morph, snap.svg, effect, animation, css, shape" />
-	<meta name="author" content="Codrops" />
-	<link rel="shortcut icon" href="../favicon.ico">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }} " />
-	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.2.0/css/font-awesome.min.css') }} " />
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/demo.css') }} " />
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/circlemenu.css') }} " />
-	<script src="{{ asset('js/snap.svg-min.js') }} "></script>
-	<!--[if IE]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-</head>
 <div class="row">
 	<div class="col-md-10 col-md-offset-1 well" id="solution">
 		<h2 class="text-center" id="eleven">11 Reasons To Choose Us</h2>
