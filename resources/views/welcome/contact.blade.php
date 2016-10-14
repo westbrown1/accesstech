90143d1c2e5a5c0174af09c5382af93442d4ec43
West Brown <lawsonsdad@gmail.com>
2016-10-13 21:49:58 -0400

    changed text color

--- resources/views/welcome/contact.blade.php
+++ resources/views/welcome/contact.blade.php
@@ -34,6 +34,22 @@
 </script>
 
 <script>
+function myMap() {
+  var myCenter = new google.maps.LatLng(34.193333,-77.871229);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "AccessTech.io Website design and development"
+  });
+  infowindow.open(map,marker);
+}
+</script>
+
+<script>
 var myCenter=new google.maps.LatLng(34.193333, -77.871229);
 
 function initialize()
