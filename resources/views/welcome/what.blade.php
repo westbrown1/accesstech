72dc7d6076ebba93536f78d12f44d6ddd07e4231
West Brown <lawsonsdad@gmail.com>
2016-10-13 20:07:05 -0400

    changed text color

--- resources/views/welcome/what.blade.php
+++ resources/views/welcome/what.blade.php
@@ -166,5 +166,54 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 </script>
+<script src="js/classie.js"></script>
+<script>
+	(function() {
 
+		function SVGMenu( el, options ) {
+			this.el = el;
+			this.init();
+		}
+
+		SVGMenu.prototype.init = function() {
+			this.trigger = this.el.querySelector( 'button.trigger' );
+			this.shapeEl = this.el.querySelector( 'span.morph-shape' );
+
+			var s = Snap( this.shapeEl.querySelector( 'svg' ) );
+			this.pathEl = s.select( 'path' );
+			this.paths = {
+				reset : this.pathEl.attr( 'd' ),
+				active : this.shapeEl.getAttribute( 'data-morph-active' )
+			};
+
+			this.isOpen = false;
+
+			this.initEvents();
+		};
+
+		SVGMenu.prototype.initEvents = function() {
+			this.trigger.addEventListener( 'click', this.toggle.bind(this) );
+		};
+
+		SVGMenu.prototype.toggle = function() {
+			var self = this;
+
+			if( this.isOpen ) {
+				classie.remove( this.el, 'menu--open' );
+			}
+			else {
+				setTimeout( function() { classie.add( self.el, 'menu--open' ); }, 175 );
+			}
+			
+			this.pathEl.stop().animate( { 'path' : this.paths.active }, 150, mina.easein, function() {
+				self.pathEl.stop().animate( { 'path' : self.paths.reset }, 800, mina.elastic );
+			} );
+
+			this.isOpen = !this.isOpen;
+		};
+
+		new SVGMenu( document.getElementById( 'menu' ) );
+
+	})();
+</script>
 @stop
