e8a69806454ffba1f63765fc2b78a1381f951308
West Brown <lawsonsdad@gmail.com>
2016-10-13 23:38:51 -0400

    changed text color

--- resources/views/welcome/contact.blade.php
+++ resources/views/welcome/contact.blade.php
@@ -3,6 +3,69 @@
 @section('title', '| Contact Us')
 
 @section('content')
+<style>
+	#map-canvas {
+	margin: 0;
+	padding: 0;
+	height: 400px;
+	max-width: none;
+}
+#map-canvas img {
+	max-width: none !important;
+}
+.gm-style-iw {
+	width: 350px !important;
+	top: 15px !important;
+	left: 0px !important;
+	background-color: #fff;
+	box-shadow: 0 1px 6px rgba(178, 178, 178, 0.6);
+	border: 1px solid rgba(72, 181, 233, 0.6);
+	border-radius: 2px 2px 10px 10px;
+}
+#iw-container {
+	margin-bottom: 10px;
+}
+#iw-container .iw-title {
+	font-family: 'Open Sans Condensed', sans-serif;
+	font-size: 22px;
+	font-weight: 400;
+	padding: 10px;
+	background-color: #48b5e9;
+	color: white;
+	margin: 0;
+	border-radius: 2px 2px 0 0;
+}
+#iw-container .iw-content {
+	font-size: 13px;
+	line-height: 18px;
+	font-weight: 400;
+	margin-right: 1px;
+	padding: 15px 5px 20px 15px;
+	max-height: 140px;
+	overflow-y: auto;
+	overflow-x: hidden;
+}
+.iw-content img {
+	float: right;
+	margin: 0 5px 5px 10px;	
+}
+.iw-subTitle {
+	font-size: 16px;
+	font-weight: 700;
+	padding: 5px 0;
+}
+.iw-bottom-gradient {
+	position: absolute;
+	width: 326px;
+	height: 25px;
+	bottom: 10px;
+	right: 18px;
+	background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+	background: -webkit-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+	background: -moz-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+	background: -ms-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+}
+</style>
 <!-- container -->
 <div class="container">
 	 <div class="row">
@@ -23,18 +86,138 @@
 </a>			
 		</div><!-- / west-->		
 	</div>
-	<div class="col-md-5 text-center">
+	{{-- <div class="col-md-5 text-center">
 		<div id="googleMap" style="width:500px;height:500px;"></div>		
 		<h4>Wilmington, NC  USA</h4>
-	</div>
-	</div>
+	</div> --}}
+
+	  <div id="map-canvas"/>
+
+  </div>
 </div><!-- /container -->
 
 
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDyN-Yeyr8E6vva-bsCzpXV2QEVO00gprk&callback-initMap">
+{{-- <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDyN-Yeyr8E6vva-bsCzpXV2QEVO00gprk&callback-initMap">
+</script> --}}
+<script>
+/*
+ * 5 ways to customize the Google Maps infowindow
+ * 2015 - en.marnoto.com
+ * http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
+*/
+
+// map center
+var center = new google.maps.LatLng(34.193333, -77.871229);
+
+// marker position
+var factory = new google.maps.LatLng(34.193333, -77.871229);
+
+function initialize() {
+  var mapOptions = {
+    center: center,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+  // InfoWindow content
+  var content = '<div id="iw-container">' +
+                    '<div class="iw-title">Porcelain Factory of Vista Alegre</div>' +
+                    '<div class="iw-content">' +
+                      '<div class="iw-subTitle">History</div>' +
+                      '<img src="http://maps.marnoto.com/en/5wayscustomizeinfowindow/images/vistalegre.jpg" alt="Porcelain Factory of Vista Alegre" height="115" width="83">' +
+                      '<p>Founded in 1824, the Porcelain Factory of Vista Alegre was the first industrial unit dedicated to porcelain production in Portugal. For the foundation and success of this risky industrial development was crucial the spirit of persistence of its founder, José Ferreira Pinto Basto. Leading figure in Portuguese society of the nineteenth century farm owner, daring dealer, wisely incorporated the liberal ideas of the century, having become "the first example of free enterprise" in Portugal.</p>' +
+                      '<div class="iw-subTitle">Contacts</div>' +
+                      '<p>VISTA ALEGRE ATLANTIS, SA<br>3830-292 Ílhavo - Portugal<br>'+
+                      '<br>Phone. +351 234 320 600<br>e-mail: geral@vaa.pt<br>www: www.myvistaalegre.com</p>'+
+                    '</div>' +
+                    '<div class="iw-bottom-gradient"></div>' +
+                  '</div>';
+
+  // A new Info Window is created and set content
+  var infowindow = new google.maps.InfoWindow({
+    content: content,
+
+    // Assign a maximum value for the width of the infowindow allows
+    // greater control over the various content elements
+    maxWidth: 350
+  });
+   
+  // marker options
+  var marker = new google.maps.Marker({
+    position: factory,
+    map: map,
+    title:"Porcelain Factory of Vista Alegre"
+  });
+
+  // This event expects a click on a marker
+  // When this event is fired the Info Window is opened.
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+  // Event that closes the Info Window with a click on the map
+  google.maps.event.addListener(map, 'click', function() {
+    infowindow.close();
+  });
+
+  // *
+  // START INFOWINDOW CUSTOMIZE.
+  // The google.maps.event.addListener() event expects
+  // the creation of the infowindow HTML structure 'domready'
+  // and before the opening of the infowindow, defined styles are applied.
+  // *
+  google.maps.event.addListener(infowindow, 'domready', function() {
+
+    // Reference to the DIV that wraps the bottom of infowindow
+    var iwOuter = $('.gm-style-iw');
+
+    /* Since this div is in a position prior to .gm-div style-iw.
+     * We use jQuery and create a iwBackground variable,
+     * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+    */
+    var iwBackground = iwOuter.prev();
+
+    // Removes background shadow DIV
+    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+    // Removes white background DIV
+    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+    // Moves the infowindow 115px to the right.
+    iwOuter.parent().parent().css({left: '115px'});
+
+    // Moves the shadow of the arrow 76px to the left margin.
+    iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+    // Moves the arrow 76px to the left margin.
+    iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+    // Changes the desired tail shadow color.
+    iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+    // Reference to the div that groups the close button elements.
+    var iwCloseBtn = iwOuter.next();
+
+    // Apply the desired effect to the close button
+    iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
+
+    // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+    if($('.iw-content').height() < 140){
+      $('.iw-bottom-gradient').css({display: 'none'});
+    }
+
+    // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+    iwCloseBtn.mouseout(function(){
+      $(this).css({opacity: '1'});
+    });
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 
-<script>
+{{-- <script>
 var myCenter=new google.maps.LatLng(34.193333, -77.871229);
 
 function initialize()
@@ -54,11 +237,12 @@ var marker=new google.maps.Marker({
 marker.setMap(map);
 
 var infowindow = new google.maps.InfoWindow({
-content: "<div class='map'>AccessTech.io website design and development - Wilmington, NC  28409</div>"
+content: "<div class='map'>AccessTech.io website design and development Wilmington, NC  28409</div>"
 });
 infowindow.open(map,marker);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+</script> --}}
+
 @stop
