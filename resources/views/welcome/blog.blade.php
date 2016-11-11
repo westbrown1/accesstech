66db80c965fce75a3ba752b108929ab790065df8
West Brown <lawsonsdad@gmail.com>
2016-11-11 18:03:04 -0500

    added blog

--- resources/views/welcome/blog.blade.php
+++ resources/views/welcome/blog.blade.php
@@ -4,14 +4,15 @@
 
 @section('content')
 <style>
-	#blog {
+	.blog {
 		margin-top: 0;
+		padding-top: 0;
 	}
 </style>
 	<!-- container -->
-	<div class="container">
-		 <div class="row">
-		 	<div class="col-md-8 col-md-offset-2" id="blog">
+	<div class="container blog">
+		 <div class="row blog">
+		 	<div class="col-md-8 col-md-offset-2 blog">
 		 		<div class="well">
 		 			<h3 class="text-center">Duke Blue Devils Football Team ends UNC Tarheels Chances at ACC Football Championship.</h3>
 		 			<h4 class="text-center">Could Marsha Brady have broken up with Mitch Trubisky in some sort of strange time warp brought on by that hardlined 1970's telephone? </h4>
