90e2314d39f6eb06622f2f252f366c9e1d7b89d4
West Brown <lawsonsdad@gmail.com>
2016-11-11 20:47:21 -0500

    setting up Blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,8 +3,22 @@
 @section('title', '| Blog Index')
 
 @section('content')
+<style>
+	#blog {
+		margin-top: 50px;
+		padding-top: 10px;
+	}
+</style>
 	<!-- container -->
 	<div class="container">
-		 <h1>Here Rests our blogs Index Page</h1>
+		<div class="well" id="blog">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h1>Here Rests our blogs Index Page</h1>
+					<a href="/google">Rethinking Google after PA election error</a>
+					<a href="/tarheels">Tarheel Football loses to Duke</a>
+				</div>
+			</div>	
+		</div>	 
 	</div><!-- /container -->
 @stop
