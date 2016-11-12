1bae07cf2818aa238902fd793cc3bc3e1a07d7f9
West Brown <lawsonsdad@gmail.com>
2016-11-11 21:14:22 -0500

    setting up Blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -16,7 +16,7 @@
 				<div class="col-md-6 col-md-offset-3">
 					<h1 class="text-center">Blog Index Page</h1>
 					<ol>
-						<li><a href="/google">Rethinking Google after PA election error</a></li><br>
+						<li><a href="/google">Rethinking Google after PA election error.</a></li><br>
 						<li><a href="/tarheels">Duke Blue Devils Football Team ends UNC Tarheels Chances at ACC Football Championship.</a></li>
 					</ol>
 					
