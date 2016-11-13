79eb744eb237e49bd2dbe807e4b215f1c4f45b07
West Brown <lawsonsdad@gmail.com>
2016-11-13 01:40:19 -0500

    updating

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -11,18 +11,19 @@
 </style>
 	<!-- container -->
 	<div class="container">
-		<div class="well" id="blog">
+		
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3">
-					<h1 class="text-center">Blog Index Page</h1>
-					<ol>
-						<li><a href="/google">Rethinking Google after Pennsylvania election error.</a></li>
-						<li><a href="/tarheels">Duke Blue Devils Football Team dims UNC Tarheels  Chances at 2016 ACC Football Championship.</a></li> 
-					</ol>
+					<div class="well" id="blog">
+						<h1 class="text-center">Blog Index Page</h1>
+						<ol>
+							<li><a href="/google">Rethinking Google after Pennsylvania election error.</a></li>
+							<li><a href="/tarheels">Duke Blue Devils Football Team dims UNC Tarheels  Chances at 2016 ACC Football Championship.</a></li> 
+						</ol>
 					
-					
+					</div>
 				</div>
 			</div>	
-		</div>	 
+			 
 	</div><!-- /container -->
 @stop
