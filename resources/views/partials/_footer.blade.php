a046baba595a59f61f0fb7916534c60298c8144f
West Brown <lawsonsdad@gmail.com>
2016-11-14 00:05:51 -0500

    added vuejs comments

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -10,10 +10,8 @@
 <!-- container -->
 <div class="container-fluid" id="main-footer">
     <div class="row">
-        <div class="col-md-12>
-            <div>
-                <p class="text-center">&copy; - 2016 AccessTech.io, A website design and development company in Wilmington, NC</p>
-            </div> 
+        <div class="col-md-12">            
+            <p class="text-center">&copy; - 2016 AccessTech.io, A website design and development company in Wilmington, NC</p>            
         </div>
     </div>
 </div><!-- /container -->
