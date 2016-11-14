b76a958ff42226c28d958e254226b2bd37293a1f
West Brown <lawsonsdad@gmail.com>
2016-11-13 23:42:11 -0500

    added vuejs comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     
     @yield('content')
     <div class="row">
-        <div class="col-lg-4 col-lg-offset-4">
+        <div class="col-xs-4 col-xs-offset-4">
             <div id="main-footer">
                 <p class="text-center">&copy; - 2016 AccessTech.io, A website design and development company in Wilmington, NC</p>
             </div> 
