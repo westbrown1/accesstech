80c37bf30ef3a402ebf1eef65a4e25e5046a6fd3
West Brown <lawsonsdad@gmail.com>
2016-11-13 23:55:01 -0500

    added vuejs comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,18 +22,7 @@
     
     @yield('content')
 
-    <!-- container -->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div id="main-footer">
-                    <p class="text-center">&copy; - 2016 AccessTech.io, A website design and development company in Wilmington, NC</p>
-                </div> 
-            </div>
-        </div>
-    </div><!-- /container -->
-
-  
+    @include('partials._footer')
 
 	@include('partials._js')
         <script>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,11 @@
+
+<!-- container -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div id="main-footer">
+                <p class="text-center">&copy; - 2016 AccessTech.io, A website design and development company in Wilmington, NC</p>
+            </div> 
+        </div>
+    </div>
+</div><!-- /container -->
