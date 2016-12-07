e05783fe234bccccbcba00075424be7218eebd56
West Brown <lawsonsdad@gmail.com>
2016-12-07 12:44:11 -0500

    updated homepage

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -56,9 +56,9 @@
             </div>
               <div class="col-md-4">
                 <div class="wow rotateIn">                
-                  <div class="img-thumbnail" class="text-center">
+                  <div class="img-thumbnail">
                   <a href="/contact">
-                    <img src="images/photo.jpg" alt="west" width="200" height="200"></a><br><br>
+                    <img src="images/photo.jpg" alt="west" width="200" height="200"><br><br>
                     <p class="text-center">West Brown</p>
                     <p class="text-center"><i class="fa fa-phone" aria-hidden="true"></i> 910-200-1391</p>
                     <p class="text-center"><a href="mailto:lawsonsdad@gmail.com" style="color: #333"><i class="fa fa-envelope"></i> Email West</p></a>                                  
