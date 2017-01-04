a2fcf174e3db236a7e17a437a1c33104db44120f
West Brown <lawsonsdad@gmail.com>
2017-01-04 13:16:16 -0500

    updated homepage

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -39,7 +39,7 @@
             <div class="content">                
                 <div id="large-header" class="large-header">                            
                     <canvas id="demo-canvas"></canvas>
-                    <h1 class="main-title text-center" onload="myFunction()" id="builder">Web Design by AccessTech.io</h1> 
+                    <h1 class="main-title text-center" onload="myFunction()">Web Design by AccessTech.io</h1> 
                 </div>
                 <div>
                     <h3 class="title text-center" onload="myFunction()" id="build">Let Us Build An Awesome Website For You Today.</h3>
