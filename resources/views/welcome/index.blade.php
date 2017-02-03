df50c0b071b7ad9ea4dad069e3130f5d88d02afb
westbrown1 <lawsonsdad@gmail.com>
2017-02-02 23:09:48 -0500

    updated

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        @include('partials._headForIndex')
-        @include('partials._head')
-        @include('partials._cssForIndex') 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js" integrity="sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8=" crossorigin="anonymous"></script>
-        
-        @include('partials._indexcss')
         <style>
           a {
             color: #333;
@@ -85,11 +77,17 @@
   <h3 class="text-center"><a href="/contact" id="footer-contact">Contact AccessTech.io</a>To Assist You With Your Web Design Needs.</h3>
   @include('partials._footer')
 </div>
-
+        @include('partials._headForIndex')
+        @include('partials._head')
+        @include('partials._cssForIndex') 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js" integrity="sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8=" crossorigin="anonymous"></script>
+        
+        @include('partials._indexcss')
 <script>
-  new WOW().init();
+    new WOW().init();
 </script>
-
 @include('partials._js')
 @include('partials._jsForIndex')
 
