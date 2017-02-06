b1cd74af4e30c36032407a8befe942ac23ab00fb
westbrown1 <lawsonsdad@gmail.com>
2017-02-06 07:42:06 -0500

    testing

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html ⚡>
-<html> <head> @include('partials._headForIndex') @include('partials._head') <link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css>@include('partials._indexcss') </div> <script async src=https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js integrity=sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U= crossorigin=anonymous></script><script async src=https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js integrity=sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8= crossorigin=anonymous></script><style amp-custom>
+<!DOCTYPE html>
+<html ⚡> <head> @include('partials._headForIndex') @include('partials._head') <link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css>@include('partials._indexcss') </div> <script async src=https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js integrity=sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U= crossorigin=anonymous></script><script async src=https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js integrity=sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8= crossorigin=anonymous></script><style amp-custom>
     a {
       color: black;
     }
