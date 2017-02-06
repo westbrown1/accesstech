0e7ea72aa031c31adeb7087b95fd346b2e13f042
westbrown1 <lawsonsdad@gmail.com>
2017-02-05 23:33:12 -0500

    Removed margin for ch-grid

--- resources/views/partials/_indexcss.blade.php
+++ resources/views/partials/_indexcss.blade.php
@@ -1,35 +1,64 @@
-        <style>
-          .jumbotron {
-            padding-top: 5px;
-            margin-bottom: 0;
-            padding-bottom: 70px;
-          }
-          .jumbotron p {
-            font-size: 15px;
-          }
-          #jumbo-black {
-            color: white;
-            padding: 10px 0 10px 0;
-            background-color: black;
-          }
-          .navbar-toggle {
-              border: 1px solid white;
-          }
-          #jumbo-p {
-            font-family: "Alex Brush Regular";
-            font-style: italic;
-            margin-left: 50px;
-          }
-          .main-title {
-            color: #333;
-            padding-bottom: 30px;
-          }
-          .title {
-            color: #333;
-          }
-          @media screen and (max-width: 1000px) {
-            #build {
-              display: none;
-            }
-          }
-       </style>
+<style>
+.jumbotron {
+  background-color: #83a507;
+  padding-top: 5px;
+  margin-bottom: 0;
+  padding-bottom: 70px;
+}
+.jumbotron p {
+  font-size: 15px;
+}
+#jumbo-black {
+  color: white;
+  padding: 10px 0 10px 0;
+  background-color: black;
+}
+.navbar-toggle {
+    border: 1px solid white;
+}
+#jumbo-p {
+  font-family: "Alex Brush Regular";
+  font-style: italic;
+  margin-left: 50px;
+}
+.main-title {
+  color: #333;
+  padding-bottom: 30px;
+}
+.title {
+  color: #333;
+}
+@media screen and (max-width: 1000px) {
+  #build {
+    display: none;
+  }
+}
+#build {
+  font-family: "Alex Brush Regular";
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.4);
+  position: absolute;
+  text-align: center;
+  font-size: 1.50em;
+  margin: 0;
+  top: 32.5%;
+  left: 50%;
+  color: #333;
+  -webkit-transform: translate3d(-50%,-50%,0);
+  transform: translate3d(-50%,-50%,0);
+}
+
+.clearfix {
+overflow: auto;
+}
+a {
+color: white;
+}
+.img-thumbnail {
+margin-top: 40px;
+margin-bottom: 40px;
+}
+
+#footer-contact {
+text-decoration: none;
+}
+</style>
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,81 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        @include('partials._headForIndex')
-        @include('partials._head')
-        @include('partials._cssForIndex') 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js" integrity="sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8=" crossorigin="anonymous"></script>
-        
-        @include('partials._indexcss')
-        <style>
-          a {
-            color: #333;
-          }
-        </style>
+  @include('partials._headForIndex')
+  @include('partials._head')
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js" integrity="sha256-399DNRyfIpWIy1ZV0KmEIIuIQ5sBHdLu9MBtVHrNtt8=" crossorigin="anonymous"></script>
+  
+  @include('partials._indexcss')
+  <style>
+    a {
+      color: black;
+    }
+    ul.ch-grid {
+      margin: 0;
+    }
+  </style>
 </head>
 <body>
 
-    @include('partials._nav')    
+  @include('partials._nav')    
 
-        <div class="demo-2">             
-          <div id="large-header" class="large-header">                            
-              <canvas id="demo-canvas"></canvas>
-              <!-- sep -->
-              <div class="sep">             
-                <h1 class="main-title text-center" onload="myFunction()" id="builder">Web Design by AccessTech.io</h1> 
-              </div><!-- /sep -->
-                <h3 class="title text-center" onload="myFunction()" id="build">Let Us Build An Awesome Website For You Today.</h3>
-          </div>                 
-        </div>
-       <br>
+<div class="demo-2">             
+  <div id="large-header" class="large-header">                            
+      <canvas id="demo-canvas"></canvas>
+      <!-- sep -->
+      <div class="sep">             
+        <h1 class="main-title text-center" onload="myFunction()" id="builder">Web Design by AccessTech.io</h1> 
+      </div><!-- /sep -->
+        <h3 class="title text-center" onload="myFunction()" id="build">Let Us Build An Awesome Website For You Today.</h3>
+  </div>                 
+</div>
+     <h3 class="text-center">You'll Be Glad You Selected Us!</h3>
 <!-- container -->
 <div class="container">
-
    <div class="row">
-            <div class="col-md-2">              
-            </div>
-              <div class="col-md-4">
-                <div class="wow rotateIn">          
-                  <div class="img-thumbnail">
-                    <img src="images/photo.jpg" alt="west" width="200" height="200"><br><br>
-                    <p class="text-center">West Brown</p>
-                    <p class="text-center">Website Builder</p>
-                    <p class="text-center"><a href="tel: +19102001391"><i class="fa fa-phone"></i> (910) 200-1391</a></p>
-                    <p class="text-center"><a href="mailto:jeffreywestbrown@gmail.com"><i class="fa fa-envelope"></i> Email West</a></p>
-                  </div><!-- /img-thumbnail -->               
+      <div class="col-md-4">
+        <div class="wow slideInRight">
+          <ul class="ch-grid">
+            <li>
+              <a href="/solution">
+                <div class="ch-item ch-img-1">        
+                  <div class="ch-info-wrap">
+                    <div class="ch-info">
+                      <div class="ch-info-front ch-img-1"></div>
+                      <div class="ch-info-back">
+                        <h3>Why Choose Us?</h3>
+                        <p>pattern by Josh Schott, View on Dribbble</p>
+                      </div>  
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-            <div class="col-md-4">
-              <div class="wow slideInRight">
-                <ul class="ch-grid">
-                  <li>
-                    <a href="/contact">
-                      <div class="ch-item ch-img-3">
-                        <div class="ch-info-wrap">
-                          <div class="ch-info">
-                            <div class="ch-info-front ch-img-3"></div>
-                            <div class="ch-info-back">
-                              <h3>Please Contact AccessTech.io Today</h3>
-                              <p>Pattern by Dustin Leer, View on Dribbble</p>
-                            </div>
-                          </div>
-                        </div>
+      <div class="col-md-4"> 
+        <div class="wow slideInRight">
+          <ul class="ch-grid">
+
+            <li>
+              <a href="/pricing">
+                <div class="ch-item ch-img-2">
+                  <div class="ch-info-wrap">
+                    <div class="ch-info">
+                      <div class="ch-info-front ch-img-2"></div>
+                      <div class="ch-info-back">
+                        <h3>Go To Pricing</h3>
+                        <p>pattern by Jeremy Slagle, View on Dribbble</p>
                       </div>
-                    </a>
-                  </li>
-                </ul>
-             </div>
-             <!-- col-md-2 -->
-             <div class="col-md-2">   
-             </div><!-- /col-md-2 -->
-            </div>
-          </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="wow slideInRight">
+          <ul class="ch-grid">
+            <li>
+              <a href="/contact">
+                <div class="ch-item ch-img-3">
+                  <div class="ch-info-wrap">
+                    <div class="ch-info">
+                      <div class="ch-info-front ch-img-3"></div>
+                      <div class="ch-info-back">
+                        <h3>Please Contact AccessTech.io Today</h3>
+                        <p>Pattern by Dustin Leer, View on Dribbble</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+       </div>
+      </div>
+  </div>
 </div><!-- /container -->
   <br>
-
 <!-- jumbotron -->
 <div class="jumbotron" id="jumbo-black"></div><!-- /jumbotron -->
 <!-- wilmington -->
