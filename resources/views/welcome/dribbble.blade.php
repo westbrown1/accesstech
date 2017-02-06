c3530822caf422f47d60912ea26de956665823c2
westbrown1 <lawsonsdad@gmail.com>
2017-02-06 15:51:59 -0500

    cleaned up blog

--- resources/views/welcome/dribbble.blade.php
+++ resources/views/welcome/dribbble.blade.php
@@ -1,1 +1,37 @@
-@extends('layouts.app') @section('title', '| Web Design | Load Effect') @section('content') <body class=popin> <div class=container> <div class=website> <div class=header> <div class=text>CSS Animation Build In</div> </div> <div class=grid> <div class=item>1</div> <div class=item>2</div> <div class=item>3</div> <div class=item>4</div> <div class=item>5</div> <div class=item>6</div> <div class=item>7</div> <div class=item>8</div> <div class=item>9</div> <div class=item>10</div> <div class=item>11</div> <div class=item>12</div> <div class=item>13</div> <div class=item>14</div> <div class=item>15</div> <div class=item>16</div> <div class=item>17</div> <div class=item>18</div> <div class=item>19</div> </div> <p>Masonry from dribbble.com and DevTips Vlog</p> </div> </div> </body> @stop
+@extends('layouts.app')
+
+@section('title', '| Dribbble')
+
+@section('content')
+<body class="popin">
+  <div class="container">
+    <div class="website">
+      <div class="header">
+        <div class="text">CSS Animation Build In</div>
+      </div>
+      <div class="grid">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+        <div class="item">6</div>
+        <div class="item">7</div>
+        <div class="item">8</div>
+        <div class="item">9</div>
+        <div class="item">10</div>
+        <div class="item">11</div>
+        <div class="item">12</div>
+        <div class="item">13</div>
+        <div class="item">14</div>
+        <div class="item">15</div>
+        <div class="item">16</div>
+        <div class="item">17</div>
+        <div class="item">18</div>
+        <div class="item">19</div>        
+      </div>
+      <p>Masonry from dribbble.com and DevTips Vlog</p>
+    </div>
+  </div>
+</body>
+@stop
