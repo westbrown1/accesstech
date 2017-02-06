44d31b85571ea9a296b0dc96c33514e30644501b
westbrown1 <lawsonsdad@gmail.com>
2017-02-06 07:06:10 -0500

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,2 +1,2 @@
 <!DOCTYPE html amp>
-<html lang=en> <head><title>AccessTech @yield('title')</title> @include('partials._head') </head> <body> @include('partials._nav') @yield('content') @include('partials._footer') @include('partials._js') </body> </html> 
+<html lang=en> <head><title>AccessTech @yield('title')</title> @include('partials._headForIndex')  @include('partials._indexcss') @include('partials._head') </head> <body> @include('partials._nav') @yield('content') @include('partials._footer') @include('partials._js') </body> </html> 
