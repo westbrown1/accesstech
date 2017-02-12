b371305b7a4d5d08af8e703abb4d61a35e2d79c0
westbrown1 <lawsonsdad@gmail.com>
2017-02-12 01:20:37 -0500

    updated google tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,25 @@
 <html lang=en >
 
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TPBGBZ8');</script>
+<!-- End Google Tag Manager -->
     <title>AccessTech @yield('title')</title>     
     @include('partials._head')
     @include('partials._headForIndex')
     @include('partials._indexcss') 
 </head>
 
-<body> @include('partials._nav')
+<body> 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPBGBZ8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+@include('partials._nav')
 
 @yield('content') 
 
