c22746f7ef16a7b95380e9415da3d91d41048ebc
westbrown1 <lawsonsdad@gmail.com>
2017-02-12 00:38:01 -0500

    updated keywords

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -3,6 +3,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="The best website design. AccessTech.io and West Brown are the web design company and website designer just for you.  We are located in Wilmington, NC.  Learn more about our web development.">
+<meta name="keywords" content="best, web, design, website, builder, designer, developer, development, SEO, Wordpress, Drupal, solution, Wilmington, NC, graphic, management, services, , software">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="google-site-verification" content="E0dIcNs5_CN27MpHWntn7rv5HLUST-i5epp59fUsaPA" />
 <link rel="stylesheet" href="{{ asset('css/app.css') }} ">
