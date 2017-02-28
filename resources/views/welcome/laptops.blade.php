af4703e160550f358d5d52177883e54a407de615
westbrown1 <lawsonsdad@gmail.com>
2017-02-28 01:37:48 -0500

    added Alexa products

--- resources/views/welcome/laptops.blade.php
+++ resources/views/welcome/laptops.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<style>
 	.shop {
-		margin-top: 100px;
+		margin-top: 90px;
 	}
 	</style>
 </head>
