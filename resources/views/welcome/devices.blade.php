cd15569ed219d144329ad812ca62fd86f0653657
westbrown1 <lawsonsdad@gmail.com>
2017-02-28 01:41:47 -0500

    better spacing on laptops

--- resources/views/welcome/devices.blade.php
+++ resources/views/welcome/devices.blade.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Best Watch Prices</title>	
+	<title>Lowest Alexa Prices</title>	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="We have the lowest prices for smart watches.  Enter here to shop for all technology">
-	<meta name="keywords" content="smart, watches, watch, lowest, best, internet, deals, deal, tech, technology, smart, price"><!-- Latest compiled and minified CSS -->
+	<meta name="description" content="We have the lowest prices for Amazon Alexa.  Enter here to shop for all technology">
+	<meta name="keywords" content="alexa, amazon, smart, watches, watch, lowest, best, internet, deals, deal, tech, technology, smart, price"><!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<style>
 	.shop1 {
