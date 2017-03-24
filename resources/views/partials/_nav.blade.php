6834fe6e05e6c27a7bcf1bf6b51c8e5649bb2f56
westbrown1 <lawsonsdad@gmail.com>
2017-03-24 15:11:13 -0400

    Added quotes back to navigation

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -27,15 +27,19 @@
 <!-- container -->
 <div class=container>
     <nav class="navbar navbar-fixed-top">
-        <div class=navbar-header>
-            <button type=button class="navbar-toggle collapsed" data-toggle=collapse data-target=#bs-example-navbar-collapse-1 aria-expanded=false> <span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </button> <a class=navbar-brand href=/><i>AccessTech.io</i></a>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" collapsed data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href=/><i>AccessTech.io</i></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id=bs-example-navbar-collapse-1>
-            <ul class="nav navbar-nav"> <a href=/>Home <span class=sr-only>(current)</span></a> <a href=/about>About Us</a> <a href=/contact>Contact Us</a> <a href=/pricing>Pricing</a> <a href=/solution>Why Choose Us?</a> <a href=/blog>Blog</a></ul>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav"> 
+            <a href="/>Home" <span class="sr-only"></span>Home</a> 
+            <a href="/about">About Us</a> <a href="/contact">Contact Us</a> 
+            <a href="/pricing">Pricing</a> <a href="/solution">Why Choose Us?</a> 
+            <a href="/blog">Blog</a></ul>
                        
             <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup=true aria-expanded="false">Go Shopping<span class="caret"></span></a>
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Go Shopping<span class="caret"></span></a>
                 <ul class="dropdown-menu">
                 <li><a href="/laptops">Laptop Computers</a></li>
                 <li><a href="/devices">Amazon Alexa</a></li>
@@ -43,26 +47,26 @@
                 <li><a href="/video_games">Video Games</a></li>
                 <li><a href="/watches">Smart Watches</a></li>
                 </ul>                
-            </li>
-                <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-haspopup=true aria-expanded=false>What We Can Do <span class=caret></span></a>
-                    <ul class=dropdown-menu>
-                        <li><a href=/buttons>Cool Button Styles</a></li>
-                        <li><a href=/parallax>Parallax Scrolling</a></li>
-                        <li><a href=/pricing_table>Pricing Tables</a></li>
                 </li>
-                <li role=separator class=divider></li>
-                <li><a href=/what>More Stuff</a></li>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">What We Can Do <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/buttons">Cool Button Styles</a></li>
+                        <li><a href="/parallax">Parallax Scrolling</a></li>
+                        <li><a href="/pricing_table">Pricing Tables</a></li>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li><a href="/what">More Stuff</a></li>
                 </ul>
                 </li>
-                <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-haspopup=true aria-expanded=false>Circle Hover Effects <span class=caret></span></a>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="ropdown" role="button" aria-haspopup="true" aria-expanded="false">Circle Hover Effects <span class="caret"></span></a>
                     <ul class=dropdown-menu>
-                        <li><a href=/index>Demo 1</a></li>
-                        <li><a href=/index2>Demo 2</a></li>
-                        <li><a href=/index3>Demo 3</a></li>
-                        <li><a href=/index4>Demo 4</a></li>
-                        <li><a href=/index5>Demo 5</a></li>
-                        <li><a href=/index6>Demo 6</a></li>
-                        <li><a href=/index7>Demo 7</a></li>
+                        <li><a href="/index">Demo 1</a></li>
+                        <li><a href="/index2">Demo 2</a></li>
+                        <li><a href="/index3">Demo 3</a></li>
+                        <li><a href="/index4">Demo 4</a></li>
+                        <li><a href="/index5">Demo 5</a></li>
+                        <li><a href="/index6">Demo 6</a></li>
+                        <li><a href="/index7">Demo 7</a></li>
                     </ul>
                 </li>
             </ul>
