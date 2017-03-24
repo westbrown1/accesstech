6eeadb0a8d635645c1c4efac502b20cf63f6a0ec
westbrown1 <lawsonsdad@gmail.com>
2017-03-24 18:22:46 -0400

    Fixed last dropdown

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -28,7 +28,12 @@
 <div class="container">
     <nav class="navbar navbar-fixed-top">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" collapsed data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href=/><i>AccessTech.io</i></a>
+            <button type="button" class="navbar-toggle" collapsed data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> 
+                <span class="sr-only">Toggle navigation</span> 
+                <span class="icon-bar"></span> <span class="icon-bar"></span> 
+                <span class="icon-bar"></span> 
+            </button>
+            <a class="navbar-brand" href=/><i>AccessTech.io</i></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -58,7 +63,7 @@
                 <li><a href="/what">More Stuff</a></li>
                 </ul>
                 </li>
-                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="ropdown" role="button" aria-haspopup="true" aria-expanded="false">Circle Hover Effects <span class="caret"></span></a>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Circle Hover Effects <span class="caret"></span></a>
                     <ul class=dropdown-menu>
                         <li><a href="/index">Demo 1</a></li>
                         <li><a href="/index2">Demo 2</a></li>
